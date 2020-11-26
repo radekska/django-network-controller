3f109aac0ebbf7d3078f2466e58b32a052d41ccc
rska <bp4fradek@gmail.com>
2020-11-26 22:59:59 +0100

    Adding project description on dashboard

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -5,7 +5,11 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(redirect_field_name='')
 def dashboard_view(request):
+    context = None
     if request.user.is_authenticated:
-        return render(request, 'dashboard.html')
+        if request.get_full_path() == '/dashboard/':
+            dashboard_only = True
+            context = dict(dashboard_only=dashboard_only)
+        return render(request, 'dashboard.html', context=context)
 
     return redirect('login')
