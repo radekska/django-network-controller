c3912a7966e49c0558005ad8bc5cec8b42a0f04c
rska <bp4fradek@gmail.com>
2020-12-19 10:54:20 +0100

    Adding requirements.txt and some project refactoring

--- main_app/celery.py
+++ main_app/celery.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
-
+import main_app
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
@@ -12,10 +12,10 @@ app = Celery('main_app')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: main_app.settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
